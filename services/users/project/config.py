82d180ffabeb8f27350ad6b009b9822737376304
Urjeet Patel <violentmagician.opensource@gmail.com>
2019-02-14 02:12:24 -0600

    Add database to user microservice

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,22 +1,27 @@
+import os
+
+
 class BaseConfig:
     """Base Configuration"""
 
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig:
     """Developement Configuration"""
 
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class ProductionConfig:
     """Production Configuration"""
 
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestingConfig:
     """testing Configuration"""
 
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
