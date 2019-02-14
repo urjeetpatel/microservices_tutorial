ae2b8d4423e9145f956660c5296f949eed904fad
Urjeet Patel <violentmagician.opensource@gmail.com>
2019-02-14 00:45:33 -0600

    Simple ping pong webservice

---
+++ services/users/project/config.py
@@ -0,0 +1,22 @@
+class BaseConfig:
+    """Base Configuration"""
+
+    TESTING = False
+
+
+class DevelopmentConfig:
+    """Developement Configuration"""
+
+    pass
+
+
+class ProductionConfig:
+    """Production Configuration"""
+
+    pass
+
+
+class TestingConfig:
+    """testing Configuration"""
+
+    TESTING = True
